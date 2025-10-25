0f8b1eda176b116ecc544aac91965b7358adb502
Armin Jazi <armin.gjazi@gmail.com>
2025-10-25 15:30:49 +0200

    fix formatting

--- src/tui/terminal/mod.rs
+++ src/tui/terminal/mod.rs
@@ -2,7 +2,7 @@ mod custom_terminal;
 mod lifecycle;
 
 pub use custom_terminal::Terminal;
+pub use lifecycle::HooshTerminal;
 pub use lifecycle::init_terminal;
 pub use lifecycle::resize_terminal;
 pub use lifecycle::restore_terminal;
-pub use lifecycle::HooshTerminal;
